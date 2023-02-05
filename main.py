fddcf537926174130f38305bdffd59834c41a08e
Spycrab-db <gavinyan2004@gmail.com>
2023-02-05 04:13:48 -0500

    Implement only kera

--- main.py
+++ main.py
@@ -1,130 +1,85 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import keras
-from keras.models import Sequential
-from keras.layers import Dense, Conv2D , MaxPool2D , Flatten , Dropout 
-from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
-
-from sklearn.metrics import classification_report,confusion_matrix
-
-import tensorflow as tf
-
-import cv2
 import os
-
-import numpy as np
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+ 
+img_width, img_height = 224, 224
 
 labels = ["Common Chickweed", "Scentless Mayweed"]
 img_size = 244
 
-# Function for getting data from a folder that contains folders with the labels as names
-def get_data(data_dir):
-    data =[]
-    for label in labels:
-        path = os.path.join(data_dir, label)
-        class_num = labels.index(label)
-        for img in os.listdir(path):
-            try:
-                img_arr = cv2.imread(os.path.join(path, img))[..., :: -1] #Convert to RGB
-                resized_arr = cv2.resize(img_arr, (img_size, img_size)) # Reshaping images
-                data.append([resized_arr, class_num])
-            except Exception as e:
-                print("Wonkiness happened")
-                print(e)
-    return np.array(data, object)
+train_data_dir = 'train'
+test_data_dir = 'test'
+nb_train_samples = 902
+nb_test_samples = 225
+epochs = 10
+batch_size = 16
 
-# Get data
-train = get_data("train")
-test = get_data("test")
 
-#Data Preprocessing
-x_train = []
-y_train = []
-x_val = []
-y_val = []
+if K.image_data_format() == 'channels_first':
+    input_shape = (3, img_width, img_height)
+else:
+    input_shape = (img_width, img_height, 3)
 
-for feature, label in train:
-  x_train.append(feature)
-  y_train.append(label)
 
-for feature, label in test:
-  x_val.append(feature)
-  y_val.append(label)
-
-# Normalize the data
-x_train = np.array(x_train, object) / 255
-x_val = np.array(x_val, object) / 255
-
-x_train.reshape(-1, img_size, img_size, 1)
-y_train = np.array(y_train, object)
-
-x_val.reshape(-1, img_size, img_size, 1)
-y_val = np.array(y_val, object)
-
-"""
-#Data Augmentation
-datagen = ImageDataGenerator(
-        featurewise_center=False,  # set input mean to 0 over the dataset
-        samplewise_center=False,  # set each sample mean to 0
-        featurewise_std_normalization=False,  # divide inputs by std of the dataset
-        samplewise_std_normalization=False,  # divide each input by its std
-        zca_whitening=False,  # apply ZCA whitening
-        rotation_range = 30,  # randomly rotate images in the range (degrees, 0 to 180)
-        zoom_range = 0.2, # Randomly zoom image 
-        width_shift_range=0.1,  # randomly shift images horizontally (fraction of total width)
-        height_shift_range=0.1,  # randomly shift images vertically (fraction of total height)
-        horizontal_flip = True,  # randomly flip images
-        vertical_flip=False)  # randomly flip images
-datagen.fit(x_train)
-
-#Implementing Convolution Neural Network
-
-#CNN Model with 3 layers
 model = Sequential()
-model.add(Conv2D(32,3,padding="same", activation="relu", input_shape=(244,244,3)))
-model.add(MaxPool2D())
-
-model.add(Conv2D(32, 3, padding="same", activation="relu"))
-model.add(MaxPool2D())
-
-model.add(Conv2D(64, 3, padding="same", activation="relu"))
-model.add(MaxPool2D())
-model.add(Dropout(0.4))
-
+model.add(Conv2D(32, (2, 2), input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+ 
+model.add(Conv2D(32, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+ 
+model.add(Conv2D(64, (2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+ 
 model.add(Flatten())
-model.add(Dense(128,activation="relu"))
-model.add(Dense(2, activation="softmax"))
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
 
-model.summary()
 
-#Compiling model
-opt = Adam(learning_rate=0.000001)
-model.compile(optimizer = opt , loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True) , metrics = ['accuracy'])
 
-#Training model for 500 Epochs
-history = model.fit(x = tf.convert_to_tensor(x_train, dtype=tf.float32), y = tf.convert_to_tensor(y_train, dtype=tf.float32), epochs = 10 , validation_data = (x_val, y_val))
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
 
-#Evaluating Results
-acc = history.history['accuracy']
-val_acc = history.history['val_accuracy']
-loss = history.history['loss']
-val_loss = history.history['val_loss']
 
-epochs_range = range(500)
+train_datagen = ImageDataGenerator(
+    rescale=1. / 255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True)
+ 
+test_datagen = ImageDataGenerator(rescale=1. / 255)
+ 
+train_generator = train_datagen.flow_from_directory(
+    train_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+ 
+validation_generator = test_datagen.flow_from_directory(
+    test_data_dir,
+    target_size=(img_width, img_height),
+    batch_size=batch_size,
+    class_mode='binary')
+ 
+model.fit_generator(
+    train_generator,
+    steps_per_epoch=nb_train_samples // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=nb_test_samples // batch_size)
 
-plt.figure(figsize=(15, 15))
-plt.subplot(2, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
 
-plt.subplot(2, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-plt.show()
-"""
+model.save_weights('model_saved.h5')
